b1e5da886f0450ea8439f162062f21920bbc5fcf
Abiel Isak <abizmo99@gmail.com>
2025-08-01 11:04:16 -0400

    Add cart reducers

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,35 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+        const {name, image, cost} = action.payload
+        console.log("plant.....",action.payload)
+        const itemExist = state.items.find(item => item.name === name)
+        if(itemExist){
+            itemExist.quantity++
+        }else{
+            state.items.push({name,image,cost,quantity:1})
+        }
     
     },
     removeItem: (state, action) => {
+        const {name} = action.payload
+
+        console.log("name....",state.items)
+        const itemChecked = state.items.some(item => item.name === name)
+        console.log("itemChecked....",itemChecked)
+        if(itemChecked){
+            const itemToremove = state.items.filter(item => item.name !== name)
+            return itemToremove
+        }
     },
     updateQuantity: (state, action) => {
+        const {name, quantity} = action.payload
 
+        const itemToUpdate = state.items.find(item => item.name === name)
+
+        if(itemToUpdate){
+            itemToUpdate.quantity = quantity
+        }
     
     },
   },
